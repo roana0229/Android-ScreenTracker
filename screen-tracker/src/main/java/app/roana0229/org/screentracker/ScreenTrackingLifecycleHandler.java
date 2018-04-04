6da21cb788bf39b36e78eb809f6df637d52042cc
kaoru.tsutsumishita <kaoru.tsutsumishita@livesense.co.jp>
2018-04-04 11:19:31 +0900

    不要なメソッドOverrideの削除

--- screen-tracker/src/main/java/app/roana0229/org/screentracker/ScreenTrackingLifecycleHandler.java
+++ screen-tracker/src/main/java/app/roana0229/org/screentracker/ScreenTrackingLifecycleHandler.java
@@ -2,14 +2,12 @@ package app.roana0229.org.screentracker;
 
 import android.app.Activity;
 import android.app.Application;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 
 public class ScreenTrackingLifecycleHandler extends FragmentManager.FragmentLifecycleCallbacks implements Application.ActivityLifecycleCallbacks {
@@ -80,41 +78,6 @@ public class ScreenTrackingLifecycleHandler extends FragmentManager.FragmentLife
     // ================================================================
 
     @Override
-    public void onFragmentPreAttached(FragmentManager fm, Fragment f, Context context) {
-        super.onFragmentPreAttached(fm, f, context);
-    }
-
-    @Override
-    public void onFragmentAttached(FragmentManager fm, Fragment f, Context context) {
-        super.onFragmentAttached(fm, f, context);
-    }
-
-    @Override
-    public void onFragmentPreCreated(FragmentManager fm, Fragment f, Bundle savedInstanceState) {
-        super.onFragmentPreCreated(fm, f, savedInstanceState);
-    }
-
-    @Override
-    public void onFragmentCreated(FragmentManager fm, Fragment f, Bundle savedInstanceState) {
-        super.onFragmentCreated(fm, f, savedInstanceState);
-    }
-
-    @Override
-    public void onFragmentActivityCreated(FragmentManager fm, Fragment f, Bundle savedInstanceState) {
-        super.onFragmentActivityCreated(fm, f, savedInstanceState);
-    }
-
-    @Override
-    public void onFragmentViewCreated(FragmentManager fm, Fragment f, View v, Bundle savedInstanceState) {
-        super.onFragmentViewCreated(fm, f, v, savedInstanceState);
-    }
-
-    @Override
-    public void onFragmentStarted(FragmentManager fm, Fragment f) {
-        super.onFragmentStarted(fm, f);
-    }
-
-    @Override
     public void onFragmentResumed(FragmentManager fm, Fragment f) {
         super.onFragmentResumed(fm, f);
         if (f instanceof TrackingMarker) {
@@ -164,30 +127,6 @@ public class ScreenTrackingLifecycleHandler extends FragmentManager.FragmentLife
             long exposureTime = System.currentTimeMillis() - fragmentStartedTime;
             callBack.trackEnded((TrackingMarker) f, exposureTime);
         }
-    }
-
-    @Override
-    public void onFragmentStopped(FragmentManager fm, Fragment f) {
-        super.onFragmentStopped(fm, f);
-    }
-
-    @Override
-    public void onFragmentSaveInstanceState(FragmentManager fm, Fragment f, Bundle outState) {
-        super.onFragmentSaveInstanceState(fm, f, outState);
-    }
-
-    @Override
-    public void onFragmentViewDestroyed(FragmentManager fm, Fragment f) {
-        super.onFragmentViewDestroyed(fm, f);
-    }
-
-    @Override
-    public void onFragmentDestroyed(FragmentManager fm, Fragment f) {
-    }
-
-    @Override
-    public void onFragmentDetached(FragmentManager fm, Fragment f) {
-        super.onFragmentDetached(fm, f);
     }
 
 }
