e1c85c572647c326ff516ea32c3f1a662e5b9e2b
Kaoru Tsutsumishita <roana.enter@gmail.com>
2018-04-14 22:33:19 +0900

    Add initial sample project

---
+++ example/app/src/main/java/app/roana0229/org/screentrackerexample/MainActivity.java
@@ -0,0 +1,13 @@
+package app.roana0229.org.screentrackerexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
